1de24dcf0269e7083d1673d96b5209047e7524c0
Phoenix <phoenix.chen@rayprosoft.com>
2023-10-31 12:32:44 +0800

    更新相依套件

--- MessageQueue.MongoDB.IntegrationTests/DependencyInjectionTests.cs
+++ MessageQueue.MongoDB.IntegrationTests/DependencyInjectionTests.cs
@@ -13,213 +13,213 @@ namespace MessageQueue.MongoDB.IntegrationTests;
 
 public class DependencyInjectionTests
 {
-	[Fact]
-	public void 建立MongoDBMessageQueue服務()
-	{
-		var services = new ServiceCollection();
+    [Fact]
+    public void 建立MongoDBMessageQueue服務()
+    {
+        var services = new ServiceCollection();
 
-		_ = services.AddMessageQueue()
-			.AddMongoMessageQueue(configuration => configuration
-				.DefineMessageQueue(
-					"test",
-					sp => new MongoMessageQueueBuilderOptions
-					{
-						MessageQueue = MongoMessageQueue.Default,
-						ConnectionString = "mongodb://localhost/Messaging"
-					})
-				.DeclareQueue("queue")
-				.AddHandler<StubMessageHandler>("test", TimeSpan.FromSeconds(1)));
+        _ = services.AddMessageQueue()
+            .AddMongoMessageQueue(configuration => configuration
+                .DefineMessageQueue(
+                    "test",
+                    sp => new MongoMessageQueueBuilderOptions
+                    {
+                        MessageQueue = MongoMessageQueue.Default,
+                        ConnectionString = "mongodb://localhost/Messaging"
+                    })
+                .DeclareQueue("queue")
+                .AddHandler<StubMessageHandler>("test", TimeSpan.FromSeconds(1)));
 
-		var serviceProvider = services.BuildServiceProvider();
+        var serviceProvider = services.BuildServiceProvider();
 
-		var sut = serviceProvider.GetRequiredService<MongoDBMessageQueueBuilder>();
+        var sut = serviceProvider.GetRequiredService<MongoDBMessageQueueBuilder>();
 
-		_ = sut.Build(serviceProvider);
-	}
+        _ = sut.Build(serviceProvider);
+    }
 
-	[Fact]
-	public void 建立Handler()
-	{
-		var services = new ServiceCollection();
+    [Fact]
+    public void 建立Handler()
+    {
+        var services = new ServiceCollection();
 
-		_ = services
-			.AddLogging()
-			.AddMessageQueue()
-			.AddMongoMessageQueue(configuration => configuration
-				.DefineMessageQueue(
-					"test",
-					sp => new MongoMessageQueueBuilderOptions
-					{
-						MessageQueue = MongoMessageQueue.Default,
-						ConnectionString = "mongodb://localhost/Messaging"
-					})
-				.DeclareQueue("queue")
-				.AddHandler<StubMessageHandler>("test", TimeSpan.FromSeconds(1)));
+        _ = services
+            .AddLogging()
+            .AddMessageQueue()
+            .AddMongoMessageQueue(configuration => configuration
+                .DefineMessageQueue(
+                    "test",
+                    sp => new MongoMessageQueueBuilderOptions
+                    {
+                        MessageQueue = MongoMessageQueue.Default,
+                        ConnectionString = "mongodb://localhost/Messaging"
+                    })
+                .DeclareQueue("queue")
+                .AddHandler<StubMessageHandler>("test", TimeSpan.FromSeconds(1)));
 
-		var serviceProvider = services.BuildServiceProvider();
+        var serviceProvider = services.BuildServiceProvider();
 
-		var messageQueueBuilder = serviceProvider.GetRequiredService<MongoDBMessageQueueBuilder>();
+        var messageQueueBuilder = serviceProvider.GetRequiredService<MongoDBMessageQueueBuilder>();
 
-		var sut = messageQueueBuilder.Build(serviceProvider);
+        var sut = messageQueueBuilder.Build(serviceProvider);
 
-		sut.Start();
+        sut.Start();
 
-		var actual = sut.Processors.First().Configuration.SubscriberFactory();
+        var actual = sut.Processors.First().Configuration.SubscriberFactory();
 
-		Assert.NotNull(actual);
-	}
+        Assert.NotNull(actual);
+    }
 
-	[Fact]
-	public void 可以接收到MongoDB的Message()
-	{
-		var services = new ServiceCollection();
+    [Fact]
+    public void 可以接收到MongoDB的Message()
+    {
+        var services = new ServiceCollection();
 
-		_ = services
-			.AddLogging()
-			.AddMessageQueue()
-			.AddMongoMessageQueue(configuration => configuration
-				.DefineMessageQueue(
-					"test",
-					sp => new MongoMessageQueueBuilderOptions
-					{
-						MessageQueue = MongoMessageQueue.Default,
-						ConnectionString = "mongodb://localhost/Messaging"
-					})
-				.DeclareQueue("queue")
-				.AddHandler<StubMessageHandler>("test", TimeSpan.FromSeconds(1)));
+        _ = services
+            .AddLogging()
+            .AddMessageQueue()
+            .AddMongoMessageQueue(configuration => configuration
+                .DefineMessageQueue(
+                    "test",
+                    sp => new MongoMessageQueueBuilderOptions
+                    {
+                        MessageQueue = MongoMessageQueue.Default,
+                        ConnectionString = "mongodb://localhost/Messaging"
+                    })
+                .DeclareQueue("queue")
+                .AddHandler<StubMessageHandler>("test", TimeSpan.FromSeconds(1)));
 
-		var serviceProvider = services.BuildServiceProvider();
+        var serviceProvider = services.BuildServiceProvider();
 
-		var messageQueueBuilder = serviceProvider.GetRequiredService<MongoDBMessageQueueBuilder>();
+        var messageQueueBuilder = serviceProvider.GetRequiredService<MongoDBMessageQueueBuilder>();
 
-		var manager = messageQueueBuilder.Build(serviceProvider);
+        var manager = messageQueueBuilder.Build(serviceProvider);
 
-		manager.Start();
+        manager.Start();
 
-		var sut = manager.Processors.First().Configuration.SubscriberFactory();
+        var sut = manager.Processors.First().Configuration.SubscriberFactory();
 
-		var container = Substitute.For<IQueueContainer>();
+        var container = Substitute.For<IQueueContainer>();
 
-		var actual = sut.Process(
-			new ProcessContext(
-				new MongoDBMessage
-				{
-					Data = new BsonDocument()
-				},
-				container));
+        var actual = sut.Process(
+            new ProcessContext(
+                new MongoDBMessage
+                {
+                    Data = new BsonDocument()
+                },
+                container));
 
-		Assert.Equal(MessageResult.Successful, actual);
-	}
+        Assert.Equal(MessageResult.Successful, actual);
+    }
 
-	[Fact]
-	public void 可以取得MongoDBMessageQueueBuilders()
-	{
-		var services = new ServiceCollection();
+    [Fact]
+    public void 可以取得MongoDBMessageQueueBuilders()
+    {
+        var services = new ServiceCollection();
 
-		_ = services.AddMessageQueue()
-			.AddMongoMessageQueue(configuration =>
-			{
-				_ = configuration.DefineMessageQueue(
-					"test1",
-					sp => new MongoMessageQueueBuilderOptions
-					{
-						MessageQueue = MongoMessageQueue.Default,
-						ConnectionString = "mongodb://localhost/Messaging"
-					});
+        _ = services.AddMessageQueue()
+            .AddMongoMessageQueue(configuration =>
+            {
+                _ = configuration.DefineMessageQueue(
+                    "test1",
+                    sp => new MongoMessageQueueBuilderOptions
+                    {
+                        MessageQueue = MongoMessageQueue.Default,
+                        ConnectionString = "mongodb://localhost/Messaging"
+                    });
 
-				_ = configuration.DefineMessageQueue(
-					"test2",
-					sp => new MongoMessageQueueBuilderOptions
-					{
-						MessageQueue = MongoMessageQueue.Default,
-						ConnectionString = "mongodb://localhost/Messaging"
-					});
-			});
+                _ = configuration.DefineMessageQueue(
+                    "test2",
+                    sp => new MongoMessageQueueBuilderOptions
+                    {
+                        MessageQueue = MongoMessageQueue.Default,
+                        ConnectionString = "mongodb://localhost/Messaging"
+                    });
+            });
 
-		var sut = services.BuildServiceProvider();
+        var sut = services.BuildServiceProvider();
 
-		var actual = sut.GetServices<MongoDBMessageQueueBuilder>();
+        var actual = sut.GetServices<MongoDBMessageQueueBuilder>();
 
-		Assert.Equal(2, actual.Count());
-	}
+        Assert.Equal(2, actual.Count());
+    }
 
-	[Fact]
-	public async Task 可以發送MongoDBMessage()
-	{
-		var services = new ServiceCollection();
+    [Fact]
+    public async Task 可以發送MongoDBMessage()
+    {
+        var services = new ServiceCollection();
 
-		var fakeManager = Substitute.For<IQueueManager>();
+        var fakeManager = Substitute.For<IQueueManager>();
 
-		var mongoMessageQueue = new MongoMessageQueue(fakeManager);
+        var mongoMessageQueue = new MongoMessageQueue(fakeManager);
 
-		_ = services
-			.AddLogging()
-			.AddMessageQueue()
-			.AddMongoMessageQueue(configuration => configuration
-				.DefineMessageQueue(
-					"test",
-					sp => new MongoMessageQueueBuilderOptions
-					{
-						MessageQueue = MongoMessageQueue.Default,
-						ConnectionString = "mongodb://localhost/Messaging"
-					})
-				.DeclareQueue("queue"))
-			.AddMongoGlobPatternExchange("*", "test", "queue");
+        _ = services
+            .AddLogging()
+            .AddMessageQueue()
+            .AddMongoMessageQueue(configuration => configuration
+                .DefineMessageQueue(
+                    "test",
+                    sp => new MongoMessageQueueBuilderOptions
+                    {
+                        MessageQueue = MongoMessageQueue.Default,
+                        ConnectionString = "mongodb://localhost/Messaging"
+                    })
+                .DeclareQueue("queue"))
+            .AddMongoGlobPatternExchange("*", "test", "queue");
 
-		var serviceProvider = services.BuildServiceProvider();
+        var serviceProvider = services.BuildServiceProvider();
 
-		foreach (var builder in serviceProvider.GetServices<MongoDBMessageQueueBuilder>())
-			_ = builder.Build(serviceProvider);
+        foreach (var builder in serviceProvider.GetServices<MongoDBMessageQueueBuilder>())
+            _ = builder.Build(serviceProvider);
 
-		var sut = serviceProvider.GetRequiredService<IMessageSender>();
+        var sut = serviceProvider.GetRequiredService<IMessageSender>();
 
-		await sut.PublishAsync("subject.test", new { Data = "Test" }.ToBson()).ConfigureAwait(false);
-	}
+        await sut.PublishAsync("subject.test", new { Data = "Test" }.ToBson());
+    }
 
-	[Fact]
-	public async Task 當有多種交換器時也可以正常發送MongoDB訊息()
-	{
-		var services = new ServiceCollection();
+    [Fact]
+    public async Task 當有多種交換器時也可以正常發送MongoDB訊息()
+    {
+        var services = new ServiceCollection();
 
-		var fakeManager = Substitute.For<IQueueManager>();
+        var fakeManager = Substitute.For<IQueueManager>();
 
-		var mongoMessageQueue = new MongoMessageQueue(fakeManager);
+        var mongoMessageQueue = new MongoMessageQueue(fakeManager);
 
-		_ = services
-			.AddLogging()
-			.AddMessageQueue()
-			.AddMongoMessageQueue(configuration =>
-			{
-				_ = configuration
-					.DefineMessageQueue(
-						"test1",
-						sp => new MongoMessageQueueBuilderOptions
-						{
-							MessageQueue = MongoMessageQueue.Default,
-							ConnectionString = "mongodb://localhost/Messaging"
-						})
-					.DeclareQueue("queue1");
+        _ = services
+            .AddLogging()
+            .AddMessageQueue()
+            .AddMongoMessageQueue(configuration =>
+            {
+                _ = configuration
+                    .DefineMessageQueue(
+                        "test1",
+                        sp => new MongoMessageQueueBuilderOptions
+                        {
+                            MessageQueue = MongoMessageQueue.Default,
+                            ConnectionString = "mongodb://localhost/Messaging"
+                        })
+                    .DeclareQueue("queue1");
 
-				_ = configuration
-					.DefineMessageQueue(
-						"test2",
-						sp => new MongoMessageQueueBuilderOptions
-						{
-							MessageQueue = MongoMessageQueue.Default,
-							ConnectionString = "mongodb://localhost/Messaging"
-						})
-					.DeclareQueue("queue2");
-			})
-			.AddMongoGlobPatternExchange("subject.test1", "test1", "queue1")
-			.AddMongoGlobPatternExchange("subject.test2", "test2", "queue2");
+                _ = configuration
+                    .DefineMessageQueue(
+                        "test2",
+                        sp => new MongoMessageQueueBuilderOptions
+                        {
+                            MessageQueue = MongoMessageQueue.Default,
+                            ConnectionString = "mongodb://localhost/Messaging"
+                        })
+                    .DeclareQueue("queue2");
+            })
+            .AddMongoGlobPatternExchange("subject.test1", "test1", "queue1")
+            .AddMongoGlobPatternExchange("subject.test2", "test2", "queue2");
 
-		var serviceProvider = services.BuildServiceProvider();
+        var serviceProvider = services.BuildServiceProvider();
 
-		foreach (var builder in serviceProvider.GetServices<MongoDBMessageQueueBuilder>())
-			_ = builder.Build(serviceProvider);
+        foreach (var builder in serviceProvider.GetServices<MongoDBMessageQueueBuilder>())
+            _ = builder.Build(serviceProvider);
 
-		var sut = serviceProvider.GetRequiredService<IMessageSender>();
+        var sut = serviceProvider.GetRequiredService<IMessageSender>();
 
-		await sut.PublishAsync("subject.test1", new { Data = "Test" }.ToBson()).ConfigureAwait(false);
-	}
+        await sut.PublishAsync("subject.test1", new { Data = "Test" }.ToBson());
+    }
 }
